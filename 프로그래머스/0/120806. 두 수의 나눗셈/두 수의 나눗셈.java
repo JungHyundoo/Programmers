cc7013a857b6aef7a0b13f4538f4e28b59bb83b6
정현두 <jhd0817@gmail.com>
2024-03-03 20:43:04 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.22 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double solution(int num1, int num2) {
+        double answer = 0;
+        answer = Math.floor(((double)num1/num2)*1000);
+        return answer;
+    }
+}
+
